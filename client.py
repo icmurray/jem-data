81fb10a171fd60388615930d6e48323fcc702046
Ian Murray <ian.c.murray@gmail.com>
2013-03-02 11:25:04 +0000

    Tidy the client script a little.

--- client.py
+++ client.py
@@ -36,7 +36,7 @@ def main(host, port, delay):
             0xC564: 2,
     }
     
-    unit = 0x01
+    unit = 0x1
     
     min_response_time = 100
     max_response_time = 0
@@ -46,13 +46,11 @@ def main(host, port, delay):
     while True:
     
         start = time.time()
-        ##response = modbus.read_registers(client,
-        ##                                 registers=interested_registers,
-        ##                                 unit=unit)
+        response = modbus.read_registers(client,
+                                         registers=interested_registers,
+                                         unit=unit)
     
-        response = client.read_holding_registers(0xc550, 4, unit=unit)
         response_time = time.time() - start
-        #print response.read_register(0xc550)
     
         min_response_time = min(min_response_time, response_time)
         max_response_time = max(max_response_time, response_time)
@@ -60,20 +58,9 @@ def main(host, port, delay):
         N += 1
     
         print"Min: %f\tMax: %f\tAvg: %f" % (min_response_time, max_response_time, (sum_response_time/N))
-        print response
-        print response.getRegister(0)
-        ##print [ response.getRegister(addr) for addr in range(0, 125) ]
-        ##print
-        ##print response.getRegister(0)
-        ##print response.getRegister(1)
-        ##print response.getRegister(2)
-        ##print response.getRegister(3)
-        ##print "###############"
-        ##print
-    
-        ##for addr in interested_registers:
-        ##    print "[%x] %x : %d" % (unit, addr, response.read_register(addr))
-    
+        results = [ response.read_register(addr) \
+                for addr in sorted(interested_registers.keys()) ]
+        print results
         time.sleep(delay)
     client.close()
 
