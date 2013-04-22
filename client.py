e2ac00677342530fbcaafea87ff053aab51361b4
Ian Murray <ian.c.murray@gmail.com>
2013-04-22 11:43:47 +0100

    Get interesting registers for client demo

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """Low Level Dirus A40 Client
 
 Just reads a set of registers from a Dirus A40, and measures how long the
@@ -11,6 +13,7 @@ Options
     --port=<port>       server port [default: 5020]
     --delay=<delay>     the delay between requests (seconds) [default: 1.0]
     --unit=<unit>       the unit address to request [default: 0x01]
+    --table=<table>     the table to read [default: 1]
 
 """
 
@@ -21,32 +24,77 @@ import time
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 
 from jem_data.core import modbus
+import jem_data.diris.registers as diris_registers
 
 logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
-def main(host, port, delay, unit):
+DEMO_REGISTERS = {
+
+    # Table 1
+    0xC550: u'Time',
+    0xC55E: u'Frequency',
+    0xC560: u'Phase Current 1',
+    0xC566: u'Neutral Current',
+    0xC568: u'𝚺 Active Power +/-',
+    0xC56A: u'𝚺 Reactive Power +/-',
+    0xC56C: u'𝚺 Apparant Power',
+
+    # Table 2
+    0xC650: u'Time',
+    0xC652: u'Active Energy +',
+    0xC654: u'Reactive Energy +',
+    0xC656: u'Apparent apparente',
+    0xC658: u'Active Energy -',
+    0xC66A: u'Reactive Energy -',
+    0xC65C: u'No. Compteurs d’Impulsions',
+
+    # Table 3
+    0xC75E: u'Avg I1',
+    0xC766: u'Avg 𝚺 Active Power +',
+    0xC768: u'Avg 𝚺 Active Power -',
+    0xC77A: u'Avg 𝚺 Reactive Power +',
+    0xC77C: u'Avg 𝚺 Reactive Power -',
+    0xC76E: u'Avg 𝚺 Apparant Power',
+    0xC77E: u'Max/Avg I1',
+
+    # Table 6
+    0xC956: u'THD I1',
+    0xC95A: u'Max Rank',
+    0xC95B: u'Harmonic I1 Row 3',
+    0xC95F: u'Harmonic I1 Row 5',
+    0xC963: u'Harmonic I1 Row 7',
+    0xC967: u'Harmonic I1 Row 9',
+    0xC96B: u'Harmonic I1 Row 11',
+    0xC96F: u'Harmonic I1 Row 13',
+    0xC973: u'Harmonic I1 Row 15',
+    0xC977: u'Harmonic I1 Row 17',
+    0xC97B: u'Harmonic I1 Row 19',
+    0xC97F: u'Harmonic I1 Row 21',
+}
+
+
+def main(host, port, delay, unit, table_num):
     client = ModbusClient(host, port=port)
     client.connect()
-    
-    interested_registers = {
-            0xC550: 2,
-            0xC560: 2,
-            0xC562: 2,
-            0xC564: 2,
-    }
     
     min_response_time = 100
     max_response_time = 0
     sum_response_time = 0
     N = 0
-    
+
+    register_widths = diris_registers.TABLES[table_num-1].copy()
+    register_labels = DEMO_REGISTERS.copy()
+    registers = dict((addr, register_widths[addr]) \
+                        for addr in DEMO_REGISTERS.keys() \
+                        if addr in register_widths)
+
     while True:
     
         start = time.time()
         response = modbus.read_registers(client,
-                                         registers=interested_registers,
+                                         registers=registers,
                                          unit=unit)
     
         response_time = time.time() - start
@@ -57,9 +105,12 @@ def main(host, port, delay, unit):
         N += 1
     
         print"Min: %f\tMax: %f\tAvg: %f" % (min_response_time, max_response_time, (sum_response_time/N))
-        results = [ response.read_register(addr) \
-                for addr in sorted(interested_registers.keys()) ]
-        print results
+
+        for addr in sorted(registers.keys()):
+            print u"{label:>45s}: {value:f}".format(
+                    label=register_labels[addr],
+                    value=response.read_register(addr))
+
         time.sleep(delay)
     client.close()
 
@@ -69,6 +120,7 @@ def _validate_args(raw_args):
     args['port'] = raw_args['--port']
     args['delay'] = float(raw_args['--delay'])
     args['unit'] = int(raw_args['--unit'], 16)
+    args['table_num'] = int(raw_args['--table'])
     return args
 
 if __name__ == '__main__':
