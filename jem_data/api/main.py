3d6db8c035ab27c6cbf50bd1d1e39baa44902063
Ian Murray <ian.c.murray@gmail.com>
2013-05-01 14:24:06 +0100

    A very poor attempt at hooking up data collection to the api.
    Obviously this is *not* the thing to do.  I just wanted to check
    everything hooked up alright.

--- jem_data/api/main.py
+++ jem_data/api/main.py
@@ -1,3 +1,3 @@
 if __name__ == '__main__':
     from jem_data.api import app
-    app.run()
+    app.run(debug=True)
--- jem_data/api/views.py
+++ jem_data/api/views.py
@@ -1,5 +1,12 @@
 from jem_data.api import app
 
+import jem_data.core.main as main
+
 @app.route('/')
 def index():
     return 'Hello World!'
+
+@app.route('/system/start', methods=['POST'])
+def start_system():
+    main.main()
+    return 'OK'
