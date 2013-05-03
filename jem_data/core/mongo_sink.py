74fdacbc4b4a1a4573fd9a750130714ca46c8d58
Ian Murray <ian.c.murray@gmail.com>
2013-05-03 09:19:13 +0100

    Keep measurements as one unit in mongo.
    Registers read in one consistent unit are stored together in the same
    mongodb BSON document.  This allows us to reliably see which sets of
    measurements are consistent with each other, and it *greatly* reduces
    the number of documents stored in the collection.

--- jem_data/core/mongo_sink.py
+++ jem_data/core/mongo_sink.py
@@ -7,9 +7,6 @@ import logging
 
 import pymongo
 
-MongoMessage = collections.namedtuple('MongoMessage',
-        'device table timing_info address value')
-
 MongoConfig = collections.namedtuple('MongoConfig',
         'host port database')
 
@@ -24,25 +21,15 @@ def mongo_writer(q, collection_names, mongo_config):
 
     while True:
         try:
-            result = q.get()
-            msgs = _split_result(result)
+            msg = q.get()
             for collection_name in collection_names:
-                _insert_into_collection(msgs, db[collection_name])
+                _insert_into_collection([msg], db[collection_name])
         except pymongo.errors.AutoReconnect, e:
             _log.error("Connection to mongo lost.  Auto-reconnect will be attempted")
         except pymongo.errors.ConnectionFailure, e:
             _log.error("Connection failure")
         except Exception, e:
             _log.error(e)
-
-def _split_result(result):
-    '''Splits a modbus result to a list of MongoMessage instances'''
-    return [ MongoMessage(
-                device = result.device,
-                table = result.table_id,
-                timing_info = result.timing_info,
-                address = address,
-                value = value) for (address,value) in result.values ]
 
 def _insert_into_collection(msgs, mongo_collection):
     mongo_collection.insert([_deep_asdict(msg) for msg in msgs])
