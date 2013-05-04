ec593117b0b0eb0ad18608229d0d4122f8bac74e
Ian Murray <ian.c.murray@gmail.com>
2013-05-04 22:33:47 +0100

    Refactor the table request manager.
    Into a more OO design.  This is more appropriate for the encapsulation
    required to send commands on an instruction queue.

--- jem_data/core/table_request_manager.py
+++ jem_data/core/table_request_manager.py
@@ -4,25 +4,83 @@
 
 """
 
+import collections
 import heapq
 import multiprocessing
 import time
 
 import jem_data.core.messages as messages
 
-def start_manager(queues, config, instruction_queue=None):
+class TableRequestManager(multiprocessing.Process):
+
+    def __init__(self, queues, config, instructions):
+        super(TableRequestManager, self).__init__()
+        self._queues = queues.copy()
+        self._config = config.copy()
+        self._instructions = instructions
+        self._tasks = []
+
+    def run(self):
+        if not self._config:
+            raise ValueError("Empty configuration!")
+
+        now = time.time()
+        for table in self._config:
+            self._enqueue_push_table_request_task(table, now=now)
+        self._enqueue_read_instructions_task()
+
+        while True:
+            _wakeup_at(self._tasks[0][0])
+
+            (_, task) = heapq.heappop(self._tasks)
+            self._run_task(task)
+
+    def _run_task(self, task):
+        if isinstance(task, _PushTableRequestTask):
+            self._run_push_table_request_task(task)
+        elif isinstance(task, _ReadInstructionsTask):
+            self._run_read_instructions_task(task)
+        else:
+            raise ValueError("Unknown Task Type: %s" % task)
+
+    def _run_push_table_request_task(self, task):
+        device, table_id = task.device, task.table_id
+        q = self._queues[device.gateway]
+        req = messages.ReadTableMsg(
+                device = device,
+                table_id = table_id)
+        q.put(req)
+        self._enqueue_push_table_request_task((device, table_id))
+
+    def _run_read_instructions_task(self, task):
+        self._enqueue_read_instructions_task()
+
+    def _enqueue_push_table_request_task(self, table, now=None):
+        task = _PushTableRequestTask(*table)
+        delay = self._config[table]
+        self._enqueue_task(task, delay, now)
+
+    def _enqueue_read_instructions_task(self, delay=0.5, now=None):
+        task = _ReadInstructionsTask()
+        self._enqueue_task(task, delay, now)
+
+    def _enqueue_task(self, task, delay, now=None):
+        now = now or time.time()
+        heapq.heappush(self._tasks, (now + delay, task))
+
+class _ReadInstructionsTask(object):
+    __slots__ = ()
+
+_PushTableRequestTask = collections.namedtuple(
+        '_PushTableRequestTask',
+        'device table_id')
+
+def start_manager(queues, config):
     """
     Create and start a new table request manager processes.
 
     :param queues: is a mapping from `Gateway` to `Queue` objects.
     :param config: is a mapping from `(Device,int)` to floats.
-    :param instruction_queue: is an optional `Queue` that the new process will
-                              listen for command messages on.  If `None` is
-                              given, then this function will create a new
-                              `multiprocessing.Queue`.
-    :return: the `Queue` that the new `Process` will listen for command
-             messages on.  This is the same as `instruction_queue` if that is
-             given, otherwise it is the newly created `multiprocessing.Queue`.
 
     The `queues` parameter holds the input queues for each `Gateway`.  These
     are used to write new requests to.
@@ -33,44 +91,14 @@ def start_manager(queues, config, instruction_queue=None):
     The `instruction_queue` is a reference to a `Queue` that the newly created
     process will listen to command messages upon.
     """
-    if instruction_queue is None:
-        instruction_queue = multiprocessing.Queue()
-    p = multiprocessing.Process(
-            target = _run,
-            args = (queues, config, instruction_queue))
+    instruction_queue = multiprocessing.Queue()
+    p = TableRequestManager(queues, config, instruction_queue)
     p.start()
-    return instruction_queue
-
-def _run(queues, config, instruction_queue):
-
-    if not config:
-        raise ValueError("Empty configuration!")
-
-    now = time.time()
-    tasks = [ _heap_item(table, config, now) for table in config ]
-    heapq.heapify(tasks)
-
-    while True:
-        _wakeup_at(tasks[0][0])
-
-        (_, table) = heapq.heappop(tasks)
-        heapq.heappush(tasks, _heap_item(table, config))
-        _push_request(table, queues)
-
-def _heap_item(table, config, now=None):
-    now = now or time.time()
-    return (now + config[table], table)
-
-def _push_request(table, queues):
-    device, table_id = table
-    q = queues[device.gateway]
-    req = messages.ReadTableMsg(
-            device = device,
-            table_id = table_id)
-    q.put(req)
+    return p
 
 def _wakeup_at(t):
     "A rough approximation.  Absolute precision is not necessary"
     now = time.time()
     if t > now:
         time.sleep(t - now)
+
