4ef9b922abd2d7acbac569a1c50fb7a4842b228d
Ian Murray <ian.c.murray@gmail.com>
2013-05-06 10:18:58 +0100

    Use config files for api configuration

--- jem_data/api/__init__.py
+++ jem_data/api/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
+app.config.from_pyfile('../../default.cfg')
+app.config.from_envvar('JEMDATA_SETTINGS', silent=True)
 
 import jem_data.api.views
--- jem_data/api/main.py
+++ jem_data/api/main.py
@@ -1,3 +1,3 @@
 if __name__ == '__main__':
     from jem_data.api import app
-    app.run(debug=True)
+    app.run()
