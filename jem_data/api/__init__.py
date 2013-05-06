b0db5e1146f37fbecca9c5964a08d055fe91a83d
Ian Murray <ian.c.murray@gmail.com>
2013-05-06 11:52:51 +0100

    Tests for the API routing

--- jem_data/api/__init__.py
+++ jem_data/api/__init__.py
@@ -2,7 +2,7 @@ import flask
 
 import jem_data.core.main as main
 
-def app_factory():
+def app_factory(setup_system=main.setup_system):
     app = flask.Flask(__name__)
     app.config.from_pyfile('../../default.cfg')
     app.config.from_envvar('JEMDATA_SETTINGS', silent=True)
@@ -10,6 +10,6 @@ def app_factory():
     import jem_data.api.views as views
     app.register_blueprint(views.system)
 
-    app.setup_system = main.setup_system
+    app.setup_system = setup_system
 
     return app
