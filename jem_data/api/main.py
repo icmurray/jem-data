b820b87299e62201708e3bfa6b7ae8666fb9ef0c
Ian Murray <ian.c.murray@gmail.com>
2013-08-27 13:59:44 +0100

    Set the log level when running the server

--- jem_data/api/main.py
+++ jem_data/api/main.py
@@ -1,4 +1,7 @@
 if __name__ == '__main__':
+    import logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.WARNING)
     from jem_data.api import app_factory
     import jem_data.services.system_control as system_control
     app = app_factory(system_control.SystemControlService())
